df9e806db28b47cb4f9d30837457542b7bba1107
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-04-02 04:31:12 +0900

    リファクタリング
    ・Sound.hリファクタリング

--- Yamioto/Sound.h
+++ Yamioto/Sound.h
@@ -4,6 +4,20 @@
 #include "DxLib.h"
 */
 
+
+//関数原型宣言========================================================================
+void Psound( int handle, int type );						//音を鳴らす関数
+void Ssound( int handle );									//音を止める関数
+int Csound( int handle );									//音が鳴っているか確認する関数
+void Vsound( int handle, int volume );						//音量を変える関数
+void SetEnemySoundPos( VECTOR v, int handle );				//音データの３Ｄサウンド用の再生位置を設定する関数
+void SetPlayerPosAndDir( VECTOR listenerpos, VECTOR listenerdir );	//音データの３Ｄサウンド用の音が聞こえる距離を設定する関数
+void SetRadius( float radius, int handle );							//音データの３Ｄサウンド用の音が聞こえる距離を設定する関数
+void SetOneMeter( float distance );									//３Ｄ空間の１メートルに相当する距離を設定する
+//====================================================================================
+
+
+//--音を鳴らす関数
 void Psound( int handle, int type ) {
 	switch ( type ) {
 	case 0:
@@ -24,30 +38,43 @@ void Psound( int handle, int type ) {
 }
 
 
+//--音を止める関数
 void Ssound( int handle ) {
 	StopSoundMem( handle );
 }
 
+
+//--音が鳴っているか確認する関数
 int Csound( int handle ) {
 	return CheckSoundMem( handle );
 }
 
+
+//--音量を変える関数
 void Vsound( int handle, int volume ) {
 	ChangeVolumeSoundMem( volume, handle );
 }
 
+
+//--音データの３Ｄサウンド用の再生位置を設定する関数
 void SetEnemySoundPos( VECTOR v, int handle ) {
 	Set3DPositionSoundMem( v, handle );
 }
 
+
+//--３Ｄサウンドのリスナーの位置とリスナーの前方位置を設定する
 void SetPlayerPosAndDir( VECTOR listenerpos, VECTOR listenerdir ) {
 	Set3DSoundListenerPosAndFrontPos_UpVecY( listenerpos, VAdd( listenerpos, listenerdir ) );
 }
 
+
+//--音データの３Ｄサウンド用の音が聞こえる距離を設定する関数
 void SetRadius( float radius, int handle ) { 
 	Set3DRadiusSoundMem( radius, handle );
 }
 
+
+//--３Ｄ空間の１メートルに相当する距離を設定する
 void SetOneMeter( float distance ) {
 	Set3DSoundOneMetre( distance );
 }
